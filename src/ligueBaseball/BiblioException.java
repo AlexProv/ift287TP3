7a8994e8f2d4b9e112f5176df9c839ee5595953e
XutBest <tricolorejusquaubout@gmail.com>
2014-02-06 14:55:27 -0500

    ...

---
+++ src/ligueBaseball/BiblioException.java
@@ -0,0 +1,19 @@
+package biblioJDBC;
+
+/**
+ * L'exception BiblioException est levée lorsqu'une transaction est inadéquate.
+ * Par exemple -- livre inexistant
+ */
+
+public final class BiblioException extends Exception
+{
+/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+public BiblioException(String message)
+{
+super(message);
+}
+}
--- src/ligueBaseball/Main.java
+++
@@ -1,9 +0,0 @@
-package ligueBaseball;
-
-public class Main {
-
-	public static void main(String[] args) {
-		//potato
-	}
-
-}
